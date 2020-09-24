7d60162b271f337cb20e50cb60bd8dce13e62cf2
Gowtham <mkgowtham7@gmail.com>
2020-09-24 23:57:27 +0100

    text clean

--- Data Exploration.R
+++ Data Exploration.R
@@ -10,17 +10,16 @@ files_list = list.files(pattern = "*.*")
 replacePunctuation <- content_transformer(function(x) {return (gsub("[[:punct:]]"," ", x))})
 for (i in files_list){
     mystring <- read_file(i)
-    documents = gsub("\\n", " ", mystring)
+    newtext <- lapply(mystring, function(x) gsub(" +", " ", mystring))
+    documents = gsub("\\n", " ", newtext)
     documents <- Corpus(VectorSource(documents))
+    documents = tm_map(documents, stripWhitespace)
     documents = tm_map(documents, content_transformer(tolower))
     documents = tm_map(documents, replacePunctuation)
     documents = tm_map(documents, removeNumbers)
-    documents = tm_map(documents, stripWhitespace)
     documents = tm_map(documents, removeWords, stopwords("english"))
-    
 }
 
 dc = strsplit(documents$content, " ")
-data.frame(table(unlist(strsplit(tolower(dc), " "))))
-
-
+data.frame(table(unlist(dc)))
+documents[1]$content
