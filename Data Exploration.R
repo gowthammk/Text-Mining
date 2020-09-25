1f8106b3b99c77d5260a285e657fce7c663495ab
Gowtham <mkgowtham7@gmail.com>
2020-09-25 20:30:39 +0100

    Document

--- Data Exploration.R
+++ Data Exploration.R
@@ -4,22 +4,30 @@ library(tm)
 library(stringr)
 #install.packages("readr") # you only need to do this one time on your system
 library(readr)
-
+df = data.frame(matrix(nrow = 400))
+colnames(df)
+count = 1
 setwd("C:/Users/Gowtham/OneDrive/Documents/Text Mining/Newsgroups/comp.sys.ibm.pc.hardware")
 files_list = list.files(pattern = "*.*")
 replacePunctuation <- content_transformer(function(x) {return (gsub("[[:punct:]]"," ", x))})
 for (i in files_list){
     mystring <- read_file(i)
-    newtext <- lapply(mystring, function(x) gsub(" +", " ", mystring))
-    documents = gsub("\\n", " ", newtext)
-    documents <- Corpus(VectorSource(documents))
-    documents = tm_map(documents, stripWhitespace)
-    documents = tm_map(documents, content_transformer(tolower))
-    documents = tm_map(documents, replacePunctuation)
-    documents = tm_map(documents, removeNumbers)
-    documents = tm_map(documents, removeWords, stopwords("english"))
+    mystring <- DocumentTermMatrix(mystring)
+    clean_corp <- clean_corpus(mystring)
 }
 
-dc = strsplit(documents$content, " ")
-data.frame(table(unlist(dc)))
-documents[1]$content
+clean_corpus <-  function(corpus){
+  documents = gsub("\\n", " ", mystring)
+  documents <- Corpus(VectorSource(documents))
+  documents = tm_map(documents, stripWhitespace)
+  documents = tm_map(documents, content_transformer(tolower))
+  documents = tm_map(documents, replacePunctuation)
+  documents = tm_map(documents, removeNumbers)
+  documents = tm_map(documents, removeWords, stopwords("english"))
+  documents = str_squish(documents$content)
+  return(documents)
+  
+}
+
+coffee_dtm <- DocumentTermMatrix(documents)
+
