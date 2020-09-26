6bb8cbda22e415f72bec693609c48156624a099d
Gowtham <mkgowtham7@gmail.com>
2020-09-26 15:19:34 +0100

    Final task 1 commit

--- Data Exploration.R
+++ Data Exploration.R
@@ -1,33 +1,62 @@
+#Author - Gowtham Kumaresh
 # Text Mining Data Exploration
+#Exploration of the dataset
+#Task 1 and 2
+#install.packages("readr") # you only need to do this one time on your system
+library(readr)
 library(tokenizers)
 library(tm)
 library(stringr)
-#install.packages("readr") # you only need to do this one time on your system
-library(readr)
-df = data.frame(matrix(nrow = 400))
-colnames(df)
-count = 1
-setwd("C:/Users/Gowtham/OneDrive/Documents/Text Mining/Newsgroups/comp.sys.ibm.pc.hardware")
-files_list = list.files(pattern = "*.*")
-replacePunctuation <- content_transformer(function(x) {return (gsub("[[:punct:]]"," ", x))})
-for (i in files_list){
-    mystring <- read_file(i)
-    mystring <- DocumentTermMatrix(mystring)
-    clean_corp <- clean_corpus(mystring)
+
+#Setting up Working directory
+setwd("C:/Users/Gowtham/OneDrive/Documents/Text Mining/Newsgroups/")
+#Cleaning of text using tm library
+cleaned_corpus <- function(clean_corp){
+  clean_corp = tm_map(clean_corp, stripWhitespace)
+  clean_corp = tm_map(clean_corp, content_transformer(tolower))
+  replacePunctuation <- content_transformer(function(x) {return (gsub("[[:punct:]]"," ", x))})
+  clean_corp = tm_map(clean_corp, replacePunctuation)
+  clean_corp = tm_map(clean_corp, removeNumbers)
+  clean_corp = tm_map(clean_corp, removeWords, stopwords("english"))
+  return(clean_corp)
 }
 
-clean_corpus <-  function(corpus){
-  documents = gsub("\\n", " ", mystring)
-  documents <- Corpus(VectorSource(documents))
-  documents = tm_map(documents, stripWhitespace)
-  documents = tm_map(documents, content_transformer(tolower))
-  documents = tm_map(documents, replacePunctuation)
-  documents = tm_map(documents, removeNumbers)
-  documents = tm_map(documents, removeWords, stopwords("english"))
-  documents = str_squish(documents$content)
-  return(documents)
-  
-}
+#Creating a string of all the 400 files in 4 folders
+#and applying text preprocessing techniques
+newstring <- ""
+folder_list = list.files(pattern = "*.*")
+for (i in 1:length(folder_list)) {
+  setwd(file.path("C:/Users/Gowtham/OneDrive/Documents/Text Mining/Newsgroups/", folder_list[i]))
+  files_list = list.files(pattern = "*.*")
+  for (j in files_list){
+    mystring <- read_file(j)
+    newstring <- paste(newstring,mystring, sep = " ")
+    documents = gsub("\\n", " ", newstring)
+    clean_corpus <- Corpus(VectorSource(documents))
+    clean_corp <- cleaned_corpus(clean_corpus)
+    dtm <- DocumentTermMatrix(clean_corp)
+    dtm2 <- as.matrix(dtm)
+  }
+} 
 
-coffee_dtm <- DocumentTermMatrix(documents)
+freq <- colSums(dtm2)
+sorted_freq <- sort(freq, decreasing = T)
+
+#Task 1 to create top 200 words and visualize using word cloud
+#install.packages("wordcloud")
+library(wordcloud)
+words <- names(sorted_freq)
+head(words)
+wordcloud(words[1:200], sorted_freq[1:200])
+
+#Task 2 Repeat the previous step, but now filter tokens by 
+#length (min. size = 4 and max. size = 20). 
+#Please indicate the total occurrences of the words
+df1 = data.frame(nrows = 1)
+for (i in names(sorted_freq)){
+  if (str_length(i) >= 4  & str_length(i) <= 20){
+    df1[i] <- sorted_freq[[i]]
+  }
+}
+df1 = df1[-1]
 
