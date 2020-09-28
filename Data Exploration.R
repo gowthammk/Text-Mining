4dd3305c53b3649b1b3a573869b3d7c6cdb6e523
Gowtham <mkgowtham7@gmail.com>
2020-09-28 23:40:17 +0100

    Naive Bayes

--- Data Exploration.R
+++ Data Exploration.R
@@ -10,6 +10,8 @@ library(tm)
 library(stringr)
 library(wordcloud)
 library(readxl)
+library(ISLR)
+library(caret)
 
 
 ##Setting up work directory
@@ -29,6 +31,7 @@ cleaned_corpus <- function(clean_corp){
   clean_corp = tm_map(clean_corp, replacePunctuation)
   clean_corp = tm_map(clean_corp, removeNumbers)
   clean_corp = tm_map(clean_corp, removeWords, stopwords("english"))
+  clean_corp = tm_map(clean_corp, removeWords, c(stopwords("english"),"else", "ifelse", "for", "in")) 
   return(clean_corp)
 }
 
@@ -121,7 +124,6 @@ BagOfWords <- function(words) {
       documents = gsub("\\n", " ", mystring)
       documet_clean_corpus <- Corpus(VectorSource(mystring))
       documet_clean_corpus <- cleaned_corpus(documet_clean_corpus)
-      #each_word <- tokenize_words(documet_clean_corpus$content)
       dtm_bagofwords <- DocumentTermMatrix(documet_clean_corpus)
       dtm2_bagofwords <- as.matrix(dtm_bagofwords)
       freq <- colSums(dtm2_bagofwords)
@@ -146,23 +148,147 @@ BagOfWords(words)
 #####################     BASIC EVALUATION    ######################
 ####################################################################
 
+##Setting up work directory
+setwd("C:/Users/Gowtham/OneDrive/Documents/Text Mining/Newsgroups")
 #Reading the bag of words
 print("/* Creating train and test data (run time - aproximately 1 min) */")
 my_data <- read_excel("BagOfWords.xlsx")
-#Split 70% of the dataset from training and 30% for testing
-n <- floor(0.70 * nrow(my_data))
+my_data$news_category = factor(my_data$news_category)
 ## set the seed to make your partition reproducible
 set.seed(1)
-train_index <- sample(seq_len(nrow(my_data)), size = n)
+index <- sample(nrow(my_data), size = nrow(my_data))
+#Split 70% of the dataset from training and 30% for testing
+n <- floor(0.70 * nrow(my_data))
+
+train_index <- index[1:n]
+test_index <- index[n+1:120]
+
 
 #Train and test data creation
 train <- my_data[train_index,]
-test  <- my_data[-train_index,]
+test  <- my_data[test_index,]
 #Verify the number of rows
 nrow(train)
+ncol(train)
 nrow(test)
+ncol(test)
 
+## The main objective is to classify the data
+##As we know we have created an entire last column for maintaing the folders
+##  The folders will be classified (ie) the last column is the response variable
+## Store the response variable
 
+#Extract news_category column for to train
+x_train = train[-12792]
+y_train = train$news_category
+#Extract news_category column for to measure accuracy
+x_test = test[-12792]
+y_test = test$news_category
+
+#To find accuracy of classification models
+accuracy_value <- function(x) {
+  sum(diag(x) / (sum(rowSums(x)))) * 100
+}
+
+############################################################
+###################   KNN ALGORITHM   ######################
+############################################################
+
+library(class)##Implementing KNN algorithm
+set.seed(1)
+
+k_value <- sqrt(nrow(train))
+k_value
+###Best way to select k value is by taking square root of nrow(train)
+### thus k = 16.7332, hence optimal value is considered as 17
+knn_fit <-
+  knn(x_train, x_test, cl = y_train, k = 17)
+##create confusion matrix
+confusionmat_knn <- table(knn_fit, y_test)
+accuracy_knn <- accuracy_value(confusionmat_knn)
+print(accuracy_knn)
+
+###K value also can be checked by elbow method
+###manual implementation of elbow method is as follows
+k.optimal = 1
+i = 1
+for (i in  1:30) {
+  knn_fit <- knn(x_train, x_test, cl = y_train, k = i)
+  ##create confusion matrix
+  confusionmat_knn <- table(knn_fit, y_test)
+  
+  accuracy_value <- function(x) {
+    sum(diag(x) / (sum(rowSums(x)))) * 100
+  }
+  accuracy <- accuracy_value(confusionmat_knn)
+  k.optimal[i] = accuracy
+}
+k.optimal
+#Accuracy plot
+plot(k.optimal,
+     type = "b",
+     xlab = "K- Value",
+     ylab = "Accuracy level")
+##Even the graph indicates that the optimal value will be around 16 or 17.
+##Choosing K = 17 will be the best choice for this dataset
+
+############################################################
+########## ACCURACY OF KNN is  60.83333 ####################
+############################################################
+
+############################################################
+
+############################################################
+###################   RANDOM FOREST   ######################
+############################################################
+set.seed(1)
+library(randomForest)
+cat("Random Forest Processing...\nLoading time is 3 minutes")
+#Mtry is set to 17 because for classification (sqrt(p) where p is 
+#number of variables in x)
+randomforest_fit <- randomForest(x_train, y_train, mtry = 17)
+y_pred_rf <- predict(randomforest_fit,x_test)
+confusionmat_rf_acc <- table(y_pred_rf, y_test)
+accuracy_value(confusionmat_rf_acc)
+confusionmat_rf <- confusionMatrix(y_pred_rf, y_test)
+
+#Performance Metrics
+#Sensitivity
+confusionmat_rf[["byClass"]][ , "Sensitivity"]
+#Precision 
+confusionmat_rf[["byClass"]][ , "Pos Pred Value"]
+
+############################################################
+############## ACCURACY OF RF is  81.66667 #################
+############################################################
+
+############################################################
+
+############################################################
+###################   Naive Bayes's   ######################
+############################################################
+
+library(e1071) ##Implementing Naive Bayes
+
+?naiveBayes
+nv_fit <- naiveBayes(x_train, y_train)
+Y_pred_nb <- predict(nv_fit, x_test)
+confusionmat_nb_acc <- table(Y_pred_nb, y_test)
+accuracy_value <- function(x) {
+  sum(diag(x) / (sum(rowSums(x)))) * 100
+}
+accuracy_value(confusionmat_nb_acc)
+confusionmat_nb <- confusionMatrix(Y_pred_nb, y_test)
+
+#Performance Metrics
+#Sensitivity
+confusionmat_nb[["byClass"]][ , "Sensitivity"]
+#Precision 
+confusionmat_nb[["byClass"]][ , "Pos Pred Value"]
+
+############################################################
+############## ACCURACY OF RF is  27.5  ####################
+############################################################
 
 
 
