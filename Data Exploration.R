9e8b65b6d104f09e21e569d254a868fe832821ac
Gowtham <mkgowtham7@gmail.com>
2020-10-02 22:19:17 +0100

    Hyper parameter tuning and feature selection

--- Data Exploration.R
+++ Data Exploration.R
@@ -33,9 +33,10 @@ DataExploration <- function() {
   print("/** Takes approximately 6 mins for word cloud creation **/")
   newstring <- ""
   folder_list = list.dirs("C:/Users/Gowtham/OneDrive/Documents/Text Mining/Newsgroups")
+  
   for (i in (folder_list)) {
     setwd(file.path(i))
-    files_list = list.files(pattern = "*.*")
+    files_list = setdiff(list.files(), list.dirs(recursive = FALSE, full.names = FALSE))
     for (j in files_list) {
       mystring <- read_file(j)
       newstring <- paste(newstring, mystring, sep = " ")
@@ -131,7 +132,7 @@ BagOfWords <- function(words) {
 
 words = DataExploration()
 BoW = BagOfWords(words)
-write.csv(BoW,"C:/Users/Gowtham/OneDrive/Documents/Text Mining/Newsgroups/BoW.csv", row.names = F)
+write.csv(BoW,"C:/Users/Gowtham/OneDrive/Documents/Text Mining/BoW.csv", row.names = F)
 print("/* Bag of words being created and CSV is stored locally*/")
 
 
@@ -140,7 +141,7 @@ print("/* Bag of words being created and CSV is stored locally*/")
 ####################################################################
 
 ##Setting up work directory
-setwd("C:/Users/Gowtham/OneDrive/Documents/Text Mining/Newsgroups")
+setwd("C:/Users/Gowtham/OneDrive/Documents/Text Mining")
 #Reading the bag of words
 print("/* Creating train and test data (run time - aproximately 1 min) */")
 my_data <- read.csv("BoW.csv", header = T)
@@ -187,41 +188,15 @@ accuracy_value <- function(x) {
 
 library(class)##Implementing KNN algorithm
 set.seed(1)
-
-###K value also can be checked by elbow method
-###manual implementation of elbow method is as follows
-k.optimal = 1
-i = 1
-for (i in  1:30) {
-  knn_fit <- knn(x_train, x_test, cl = y_train, k = i)
-  ##create confusion matrix
-  confusionmat_knn <- table(knn_fit, y_test)
-  
-  accuracy_value <- function(x) {
-    sum(diag(x) / (sum(rowSums(x)))) * 100
-  }
-  accuracy <- accuracy_value(confusionmat_knn)
-  k.optimal[i] = accuracy
-}
-k.optimal
-
-#Accuracy plot
-plot(k.optimal,
-     type = "b",
-     xlab = "K- Value",
-     ylab = "Accuracy level")
-##Even the graph indicates that the optimal value will be around 16 or 17.
-##Choosing K = 17 will be the best choice for this dataset
-set.seed(1)
 knn_fit <-
-  knn(x_train, x_test, cl = y_train, k = 25)
+  knn(x_train, x_test, cl = y_train, k = 1)
 ##create confusion matrix
 confusionmat_knn <- table(knn_fit, y_test)
 accuracy_knn <- accuracy_value(confusionmat_knn)
 print(accuracy_knn)
 
 ############################################################
-########## ACCURACY OF KNN is  50 ########################
+########## ACCURACY OF KNN is  45.8333 #####################
 ############################################################
 
 ############################################################
@@ -231,10 +206,11 @@ print(accuracy_knn)
 ############################################################
 set.seed(1)
 library(randomForest)
+library(caret)
 cat("Random Forest Processing...\nLoading time is 3 minutes")
 #Mtry is set to 17 because for classification (sqrt(p) where p is 
 #number of variables in x)
-randomforest_fit <- randomForest(x_train, y_train, mtry = 17)
+randomforest_fit <- randomForest(x_train, y_train)
 y_pred_rf <- predict(randomforest_fit,x_test)
 confusionmat_rf_acc <- table(y_pred_rf, y_test)
 accuracy_value(confusionmat_rf_acc)
@@ -247,7 +223,7 @@ confusionmat_rf[["byClass"]][ , "Sensitivity"]
 confusionmat_rf[["byClass"]][ , "Pos Pred Value"]
 
 ############################################################
-############## ACCURACY OF RF is  66.666667 ################
+################## ACCURACY OF RF is  90 ###################
 ############################################################
 
 ############################################################
@@ -296,9 +272,10 @@ cleaned_corpus <- function(clean_corp){
   clean_corp = tm_map(clean_corp, replacePunctuation)
   clean_corp = tm_map(clean_corp, removeNumbers)
   clean_corp = tm_map(clean_corp, removeWords, stopwords("english"))
-  clean_corp = tm_map(clean_corp, removeWords, c(stopwords("english"),"else", "ifelse", "for", "in")) 
+  clean_corp = tm_map(clean_corp, removeWords, c(stopwords("english"),"else", "ifelse", "for", "in", "shadowmask")) 
   return(clean_corp)
 }
+
 
 preprocessing <- function() {
   #Creating a string of all the 400 files in 4 folders
@@ -308,7 +285,8 @@ preprocessing <- function() {
   folder_list = list.dirs("C:/Users/Gowtham/OneDrive/Documents/Text Mining/Newsgroups")
   for (i in (folder_list)) {
     setwd(file.path(i))
-    files_list = list.files(pattern = "*.*")
+    files_list = setdiff(list.files(),
+                         list.dirs(recursive = FALSE, full.names = FALSE))
     for (j in files_list) {
       mystring <- read_file(j)
       newstring <- paste(newstring, mystring, sep = " ")
@@ -397,7 +375,7 @@ PreprocessedBagOfWords <- function(words) {
     }
   }
   bag_of_words[is.na(bag_of_words)] = 0
-  write_xlsx(bag_of_words,"C:/Users/Gowtham/OneDrive/Documents/Text Mining/Newsgroups/PreProcessBagOfWords.xlsx")
+  write_xlsx(bag_of_words,"C:/Users/Gowtham/OneDrive/Documents/Text Mining/PreProcessBagOfWords.xlsx")
   print("/* Bag of words being created and XLSX is stored locally*/")
 }
 
@@ -407,3 +385,172 @@ PreprocessedBagOfWords(preprocessedwords)
 ############################################################
 ############## Hyper Paramerter Tuning ####################
 ############################################################
+
+##Setting up work directory
+setwd("C:/Users/Gowtham/OneDrive/Documents/Text Mining/")
+#Reading the bag of words
+print("/* Creating train and test data (run time - aproximately 1 min) */")
+my_data <- read_excel("PreProcessBagOfWords.xlsx")
+my_data$news_category = factor(my_data$news_category)
+## set the seed to make your partition reproducible
+set.seed(1)
+index <- sample(nrow(my_data), size = nrow(my_data))
+#Split 70% of the dataset from training and 30% for testing
+n <- floor(0.70 * nrow(my_data))
+
+train_index <- index[1:n]
+test_index <- index[n+1:120]
+
+
+#Train and test data creation
+train <- my_data[train_index,]
+test  <- my_data[test_index,]
+#Verify the number of rows
+nrow(train)
+ncol(train)
+nrow(test)
+ncol(test)
+
+## The main objective is to classify the data
+##As we know we have created an entire last column for maintaing the folders
+##  The folders will be classified (ie) the last column is the response variable
+## Store the response variable
+
+#Extract news_category column for to train
+x_train = train[-12790]
+y_train = train$news_category
+#Extract news_category column for to measure accuracy
+x_test = test[-12790]
+y_test = test$news_category
+
+#To find accuracy of classification models
+accuracy_value <- function(x) {
+  sum(diag(x) / (sum(rowSums(x)))) * 100
+}
+
+#Feature selection using Boruta
+
+library(Boruta)
+library(randomForest)
+library(caret)
+str(my_data)
+
+
+set.seed(1)
+boruta <- Boruta(news_category ~ .,data = my_data, doTrace = 2)
+print(boruta)
+plot(boruta, las = 2, cex.axis = 0.7)
+
+bor <- TentativeRoughFix(boruta)
+print(bor)
+attStats(boruta)
+
+set.seed(1)
+getNonRejectedFormula(boruta)
+getConfirmedFormula(boruta)
+
+###########################################################
+#################   Feature Selection  ####################
+###########################################################
+
+#New dataset 
+fs_data <- my_data[c("cmu", "srv", "cantaloupe", "talk", "politics", "misc", "writes", "article", "references", "people" ,"guns", "alt", "ibm",
+  "comp", "gun", "hardware", "sys", "xref", "electronics", "sci",   "government", "president", "usa", "soc", "clinton", "fbi", "culture", 
+  "optilink", "legal", "card", "circuit", "batf", "cramer", "men",
+  "abortion", "religion", "scsi", "port", "sex", "weapons", "atf",
+  "gay", "bios", "ide", "clayton", "firearms", "motherboard", "firearm",
+  "burns", "ranch", "six", "liberties", "dividian", "qvfik")]
+
+fs_data$new_category = rep(c(1:4), each = 100)
+fs_data$new_category = factor(fs_data$new_category)
+## set the seed to make your partition reproducible
+set.seed(1)
+index <- sample(nrow(fs_data), size = nrow(fs_data))
+
+noofrow <- floor(0.70 * nrow(fs_data))
+
+train_ind <- index[1:noofrow]
+test_ind <- index[noofrow+1:120]
+
+
+#Train and test data creation
+train <- fs_data[train_ind,]
+test  <- fs_data[test_ind,]
+
+ncol(train)
+#Extract news_category column for to train
+x_train = train[-55]
+y_train = train$new_category
+#Extract news_category column for to measure accuracy
+x_test = test[-55]
+y_test = test$new_category
+
+
+
+############### Hyperparameter Tuning ######################
+install.packages("mlr")
+library(mlr)
+
+hyp_tree <- makeLearner("classif.rpart")
+hyp_rf <- makeLearner("classif.randomForest")
+hyp_svm <- makeLearner("classif.ksvm")
+hyp_knn <- makeLearner("classif.knn")
+
+param_tree <- makeParamSet(
+  makeIntegerParam("minsplit",lower = 1, upper = 100),
+  makeIntegerParam("minbucket", lower = 5, upper = 50),
+  makeNumericParam("cp", lower = 0.001, upper = 0.2)
+)
+
+param_rf <- makeParamSet(
+  makeIntegerParam("ntree",lower = 50, upper = 500),
+  makeIntegerParam("mtry", lower = 3, upper = 10),
+  makeIntegerParam("nodesize", lower = 10, upper = 50)
+)
+
+param_svm <-  makeParamSet(
+  makeNumericParam("C", lower=-10, upper=10, trafo = function(x)2^x),
+  makeNumericParam("sigma",lower=-10,upper=10,trafo = function(x)2^x))
+
+param_knn = makeParamSet(makeIntegerParam("k",lower=1,upper=30))
+
+
+ctrl = makeTuneControlGrid()
+rdesc = makeResampleDesc("CV", iters = 3L)
+task = makeClassifTask(data=train,target="new_category")
+
+set.seed(1)
+tune_trees = tuneParams(hyp_tree,task, resampling = rdesc, par.set=param_tree, 
+                       control=ctrl, measures = acc)
+# minsplit=1; minbucket=5; cp=0.001 : acc.test.mean=0.9643484
+set.seed(1)
+tune_knn = tuneParams(hyp_knn, task, resampling = rdesc, par.set=param_knn, 
+                     control=ctrl, measures = acc) 
+#k=4 : acc.test.mean=0.9323191
+set.seed(1)
+tune_rf = tuneParams(hyp_rf, task, resampling = rdesc, par.set=param_rf, 
+                    control=ctrl, measures = acc) 
+# ntree=50; mtry=7; nodesize=10 : acc.test.mean=0.9822695
+set.seed(1)
+tune_svm = tuneParams(hyp_svm, task, resampling = rdesc, par.set=param_svm, 
+                     control=ctrl, measures = acc) 
+# C=47; sigma=0.00456 : acc.test.mean=0.9784946
+# Optimal results from hyperparameter tuning
+print(tune_trees)
+
+print(tune_knn)
+
+print(tune_rf)
+
+print(tune_svm)
+
+par(mfrow=c(2,2))
+plot(tune_trees$x$minsplit,tune_trees$x$maxdepth,xlab="Min. split",ylab="Max. Depth",col="black",
+     pch=16,main="Decision Trees")
+plot(tune_knn$x,tune_knn$y,xlab="K value",ylab="Accuracy",col="black",pch=16,
+     main="Knn")
+plot(tune_rf$x$ntree, tune_rf$x$mtry, xlab="No. of trees",ylab="mtry",main="Random Forest",
+     pch=16, col="black")
+plot(tune_svm$x$C, tune_svm$x$sigma, xlab="Cost", ylab="Sigma", main="SVM",
+     col="black",pch=16)
+
